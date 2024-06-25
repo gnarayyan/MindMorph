9509c0bdbba29ec60e3884ef383837d7a8daa1c7
Narayan Gautam <ng4111894@gmail.com>
2024-06-26 00:32:39 +0545

    Add get all courseDomain endpoint

--- backend/node-server/src/router/course/courseDomain.js
+++ backend/node-server/src/router/course/courseDomain.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { getCourseDomain, createCourseDomain, updateCourseDomain, deleteCourseDomain } = require('../../controller/course/courseDomain')
+const {
+  getAllCourseDomain,
+  getCourseDomain,
+  createCourseDomain,
+  updateCourseDomain,
+  deleteCourseDomain,
+} = require('../../controller/course/courseDomain');
 
-router.get('/:id', getCourseDomain)
-router.post('/', createCourseDomain)
-router.patch('/:id', updateCourseDomain)
-router.delete('/:id', deleteCourseDomain)
+router.get('/', getAllCourseDomain);
+router.get('/:id', getCourseDomain);
+router.post('/', createCourseDomain);
+router.patch('/:id', updateCourseDomain);
+router.delete('/:id', deleteCourseDomain);
 
-module.exports = router
+module.exports = router;
