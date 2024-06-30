83c9efc23b21a634526e03d8656eae30eb8df7c7
Narayan Gautam <ng4111894@gmail.com>
2024-06-30 15:44:03 +0545

    Update coursechoice to avoid error in frontend

--- backend/node-server/src/controller/course/courseChoice.js
+++ backend/node-server/src/controller/course/courseChoice.js
@@ -29,19 +29,20 @@ const getTrendingCourse = async (req, res) => {
     const { qty = 2 } = req.query;
 
     // Fetching top-rated products
-    const topRatedProducts = await prisma.course.findMany({
-      take: parseInt(qty), // Limit the number of products
-      orderBy: { rating: 'desc' }, // Sort by rating in descending order
-      select: {
-        id: true,
-        title: true,
-        thumbnail: true,
-        rating: true,
-        price: true,
-      },
-    });
+    // const topRatedProducts = await prisma.course.findMany({
+    //   take: parseInt(qty), // Limit the number of products
+    //   orderBy: { rating: 'desc' }, // Sort by rating in descending order
+    //   select: {
+    //     id: true,
+    //     title: true,
+    //     thumbnail: true,
+    //     rating: true,
+    //     price: true,
+    //   },
+    // });
 
-    res.json(topRatedProducts);
+    // res.json(topRatedProducts);
+    res.json([]);
   } catch (error) {
     console.error('Error fetching top-rated products:', error);
     res.status(500).json({ error: 'Internal Server Error' });
